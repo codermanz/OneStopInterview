d5eee8aa39f13a54e50acdbbc89b6edf7a2f4826
Jeremy Esch <jeremy@Jeremys-MacBook-Pro.local>
2022-10-26 15:50:07 -0700

    Added frontend react app + tailwindcss config

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+   return (
+      <>
+         <div>
+            <span>Home View</span>
+         </div>
+      </>
+   );
+}
+
+export default Home;
