3125332bd2cd7135ce74f6e46a63859522be1dcb
Catherine Kuntoro <catherinekuntoro@sjsu.edu>
2022-10-30 22:13:56 -0700

    roadmap and interview Qs

---
+++ frontend/src/components/interview-components/BehavioralInterviewComponent.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import {
+  Divider,
+  Typography,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
+import InterviewButtonContent from "./InterviewButtonContent";
+
+function TechnicalInterviewComponent() {
+  // Theme for rounded corners to make the button resemble a pill
+  const pillTheme = createTheme({
+    shape: {
+      borderRadius: 50,
+    },
+  });
+
+  /*
+    TechnicalInterviewComponent will return:
+    - Technical interview topics
+    - Pill-shaped buttons that opens an external website when clicked
+  */
+  return (
+    <div>
+      <Typography sx={{ margin: "10px" }}>Topic 1</Typography>
+      <ThemeProvider theme={pillTheme}>
+        <InterviewButtonContent
+          buttonText={"Question 1 Topic 1"}
+          buttonLink={"http://www.google.com/"}
+        />
+        <InterviewButtonContent
+          buttonText={"Question 1 Topic 2"}
+          buttonLink={"http://www.google.com/"}
+        />
+      </ThemeProvider>
+
+      <Divider sx={{ margin: "20px" }} />
+
+      <Typography sx={{ margin: "10px" }}>Topic 2</Typography>
+      <ThemeProvider theme={pillTheme}>
+        <InterviewButtonContent
+          buttonText={"Question 1 Topic 2"}
+          buttonLink={"http://www.google.com/"}
+        />
+      </ThemeProvider>
+
+      <Divider sx={{ margin: "20px" }} />
+
+      <Typography sx={{ margin: "10px" }}>Topic 2</Typography>
+      <ThemeProvider theme={pillTheme}>
+        <InterviewButtonContent
+          buttonText={"Question 1 Topic 2"}
+          buttonLink={"http://www.google.com/"}
+        />
+      </ThemeProvider>
+
+      <Divider sx={{ margin: "20px" }} />
+
+      <Typography sx={{ margin: "10px" }}>Topic 2</Typography>
+      <ThemeProvider theme={pillTheme}>
+        <InterviewButtonContent
+          buttonText={"Question 1 Topic 2"}
+          buttonLink={"http://www.google.com/"}
+        />
+      </ThemeProvider>
+
+      <Divider sx={{ margin: "20px" }} />
+
+      <Typography sx={{ margin: "10px" }}>Topic 2</Typography>
+      <ThemeProvider theme={pillTheme}>
+        <InterviewButtonContent
+          buttonText={"Question 1 Topic 2"}
+          buttonLink={"http://www.google.com/"}
+        />
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default TechnicalInterviewComponent;
--- frontend/src/pages/Roadmap.jsx
+++ frontend/src/pages/Roadmap.jsx
@@ -1,7 +1,87 @@
-import React from 'react'
+import React from "react";
+import { Footer, Navbar } from "../components/index";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 function Roadmap() {
-   return <div>Roadmap</div>
+  // Handle the state changes so only one panel in the accordion
+  // is open at a time
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  // Use dark theme components
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+
+  return (
+    <div className="bg-dark-bg h-screen text-white overflow-hidden">
+      <Navbar />
+      <Typography variant="h2" gutterBottom>
+        Frontend and Backend Roadmap
+      </Typography>
+      <Typography variant="h5" gutterBottom sx={{ marginBottom: "40px" }}>
+        Not sure where to begin your programming journey? Check out our roadmap
+        below.
+      </Typography>
+      <ThemeProvider theme={darkTheme}>
+        <Accordion
+          sx={{ width: "50%" }}
+          expanded={expanded === "frontend-panel"}
+          onChange={handleChange("frontend-panel")}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="roadmap-accordion-frontend-panel"
+          >
+            <Typography>Accordion 1</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+
+        <Accordion
+          sx={{ width: "50%" }}
+          expanded={expanded === "backend-panel"}
+          onChange={handleChange("backend-panel")}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="roadmap-accordion-backendend-panel"
+          >
+            <Typography>Accordion 2</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      </ThemeProvider>
+      <Footer />
+    </div>
+  );
 }
 
-export default Roadmap
+export default Roadmap;
