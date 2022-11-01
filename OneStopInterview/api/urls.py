6101d1cf11f15d19ac3552c78bb356c7b2ce16f1
Saad Jeelani <saad.jeelani88@gmail.com>
2022-11-01 00:40:37 -0700

    Added basic routing and view definitions with empty view definitions

--- OneStopInterview/api/urls.py
+++ OneStopInterview/api/urls.py
@@ -1,5 +1,19 @@
 from django.urls import path, include
-from .views import main
+from .views import *
 urlpatterns = [
-    path('', main)
+    path('', main),
+    path('createPost/', main, name='createPost'),
+    path('createUser/', main, name='createUser'),
+    path('getUser/<int:pk>', main, name='getUser'),
+    path('admin/addQuestion', main, name='addQuestion'),
+    path('admin/addCategory', main, name='addCategory'),
+    path('admin/deletePost/<int:pk>', main, name='deletePost'),
+    path('admin/getCategories', main, name='getCategories'),
+    path('questionsBank/', main, name='questionsBank'),
+    path('posts/', main, name='getPosts'),
+    path('posts/<int:pk>', main, name='getPost'),
+    path('userProgress/<int:pk', main, name='getUserProgress'),
+    path('modifyUserProgress/<int:pk>', main, name='modifyUserProgress'),
+    path('deletePost/<int:pk>', main, name='deletePost'),
+    path('jobPostings/', main, name='getJobPostings'),
 ]
--- OneStopInterview/api/views.py
+++ OneStopInterview/api/views.py
@@ -1,6 +1,96 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from rest_framework import generics
+
+
+class PostList(generics.ListCreateAPIView):
+    """
+        Create a post or get a list of all posts within the database that are parent posts
+        API endpoints that use this view:
+            - /posts
+            - /createPost
+   """
+    # Create queryset
+    # Serialize
+    pass
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+        Retrieve a specific post object and all its children posts
+        Will only be valid if specified post is a root post.
+        Also retrieves and responds with all reply posts to a
+        root post
+        API endpoints that use this view:
+            - /posts/{post_id}
+            - /deletePost/{post_id}
+            - /admin/deletePost/{post_id}
+   """
+    # Create queryset
+    # Serialize
+    pass
+
+
+class CreateUser(generics.CreateAPIView):
+    """
+        Creates a specified user's object
+        API endpoints that use this view:
+            - /createUser
+    """
+    pass
+
+
+class GetUser(generics.RetrieveAPIView):
+    """
+        Retrieve a user's view
+        API endpoints that use this view:
+            - /getUser/{user_id}
+    """
+    pass
+
+
+class GetQuestions(generics.ListCreateAPIView):
+    """
+        Gets a list of questions from the question bank
+        API endpoints that use this view:
+            - /questionsBank
+            - /admin/addQuestion
+    """
+    pass
+
+
+class UserProgress(generics.ListCreateAPIView):
+    """
+        Gets user progress by getting all questions completed by the user
+        Also used to mark a question completed
+        API endpoints that use this view:
+            - /userProgress/{user_id}
+            - modifyUserProgress/{user_id}
+    """
+    pass
+
+
+class JobPostings(generics.ListAPIView):
+    """
+        Gets jobs for a particular set of parameters
+        API endpoints that use this view:
+            - /jobPostings
+    """
+    pass
+
+
+class Category(generics.ListCreateAPIView):
+    """
+        Add categories or get list of all cetegories
+        API endpoints that use this view:
+            - /admin/addCategory
+            - /admin/getCategories
+    """
+    pass
+
 
 def main(request):
-   return HttpResponse("Hello World")
+    """
+
+   """
+    return HttpResponse("Hello World")
