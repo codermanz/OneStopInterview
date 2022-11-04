55854929fa55be41c55f359768d6fc9085dedaeb
Jeremy Esch <jeremy@Jeremys-MacBook-Pro.local>
2022-11-04 14:36:16 -0700

    Resolved merge conflicts + updated pathing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home, InterviewPage, Login, ResumeTips, Roadmap, Checkpoints } from "./pages";
+import {
+  Home,
+  InterviewPage,
+  Login,
+  ResumeTips,
+  Roadmap,
+  Checkpoints,
+} from "./pages";
 import { Navbar } from "./components";
 import CssBaseline from "@mui/material/CssBaseline";
 
@@ -17,12 +24,12 @@ function App() {
             <Route path="/interview" element={<InterviewPage />} />
             <Route path="/resume-tips" element={<ResumeTips />} />
             <Route path="/roadmap" element={<Roadmap />} />
+            <Route path="/resume-tips/checkpoints" element={<Checkpoints />} />
           </Routes>
         </div>
       </Router>
     </>
   );
-
 }
 
 export default App;
