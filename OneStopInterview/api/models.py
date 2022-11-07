83d6c37ee2a1a3e1383786898709dafbf2db9578
Saad Jeelani <saad.jeelani88@gmail.com>
2022-11-06 18:10:17 -0800

    Set up barebones of models and endpoints. POst model still neds sto be completed (primary key)

--- OneStopInterview/api/models.py
+++ OneStopInterview/api/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    """
+        Post model
+    """
+    class PostObjects(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(id='')
+
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE,
+                               related_name='forum_posts')
+    objects = models.Manager()
+    post_objects = PostObjects()
+
+    def __str__(self):
+        return self.title
+
+
+class UserProgress(models.Model):
+    """
+        User Progress model
+    """
+    pass
+
+
+class TechBehQuestion(models.Model):
+    """
+        Technical Behvaioural Questions bank model
+    """
+    pass
+
+
+class QuestionCategory(models.Model):
+    """
+        Category model
+    """
+    pass
+
+
