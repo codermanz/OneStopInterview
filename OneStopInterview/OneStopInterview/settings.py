7de85ee0a051a68ee62a940f32774dd73a587a5a
Alan Luu <alanluu4@gmail.com>
2022-11-11 23:11:41 -0800

    Revert

--- OneStopInterview/OneStopInterview/settings.py
+++ OneStopInterview/OneStopInterview/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-yo81-otn&qd5mp-bk6&w#q^gi02=zk6qdt2&qfma%(@r*l_&s4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 # Application definition
 
