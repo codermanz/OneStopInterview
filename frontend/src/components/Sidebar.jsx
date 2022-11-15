e525493abe251554d46966d1d052d7269967ed17
Eunbee Na <eunbee.na@sjsu.edu>
2022-11-15 11:47:37 -0800

    Fix sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,8 +2,19 @@ import React, { useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../themes/theme";
+import ProgressBar from "./Progressbar";
 
 function Sidebar(props) {
+
+  const progressList = [
+    { title: "My Progress", bgcolor: "#6a1b9a", completed: props.progress } 
+    /*
+    { title: "Behavioral Interview Questions", bgcolor: "#00695c", completed: 30 },
+    { title: "Frontend roadmap", bgcolor: "#ef6c00", completed: 53 },
+    { title: "Backend roadmap", bgcolor: "#d64161", completed: 90 },
+    */
+  ];
+
   return (
     <ThemeProvider theme={theme}>
       <Box
@@ -15,9 +26,9 @@ function Sidebar(props) {
           position: "sticky",
           top: "74px",
         }}>
-        <Typography variant="h3" sx={{ color: "white" }}>
-          {props.progress}% Completed
-        </Typography>
+          {progressList.map((item, idx) => (
+            <ProgressBar key={idx} title={item.title} bgcolor={item.bgcolor} completed={item.completed} />
+          ))}
       </Box>
     </ThemeProvider>
   );
